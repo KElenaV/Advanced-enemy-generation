a91f7054121b65d515ea28620e881986ac0f02db
Elena <foxichenochek@yandex.ru>
2023-09-13 08:50:05 -0600

    Clear code

--- Assets/Scripts/BoxMover.cs
+++ Assets/Scripts/BoxMover.cs
@@ -5,27 +5,26 @@ public class BoxMover : MonoBehaviour
     [SerializeField] private Vector3[] _points;
     [SerializeField] private float _speed = 1;
 
-    private int _pointsCount;
     private int _currentPoint = 0;
-    
-    public Vector3 TargetPoint => _points[_currentPoint];
+    private Vector3 _targetPoint;
 
     private void Start()
     {
-        _pointsCount = _points.Length;
+        _targetPoint = _points[_currentPoint];
     }
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, TargetPoint, _speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _targetPoint, _speed * Time.deltaTime);
 
-        if (transform.position == TargetPoint)
+        if (transform.position == _targetPoint)
             SetNextPoint();
     }
 
     private void SetNextPoint()
     {
         _currentPoint++;
-        _currentPoint = _currentPoint == _pointsCount ? 0 : _currentPoint;
+        _currentPoint = _currentPoint == _points.Length ? 0 : _currentPoint;
+        _targetPoint = _points[_currentPoint];
     }
 }
